4b2068cbc036db5f6691c7872d644b252c9ca3b2
RogerioAguilera <raguileraribeiro33@gmail.com>
2023-12-04 20:08:14 -0300

    update test

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose, { mongo } from "mongoose";
 
 async function conectaDatabase(){
 mongoose.connect(
-  "mongodb+srv://mark0:ss5s55ss@cluster0.gsam2wr.mongodb.net/animes?retryWrites=true&w=majority")
+  "mongodb+srv://mark0:@cluster0.gsam2wr.mongodb.net/animes?retryWrites=true&w=majority")
   return mongoose.connection;
 }
 
